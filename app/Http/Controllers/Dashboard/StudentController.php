d8a3fa0b4ab21b8ebe60bdc882087e73ba99b7f0
Kamil <kamil.piech.97@gmail.com>
2023-09-17 10:03:47 +0200

    #5 - wip: crud for students

---
+++ app/Http/Controllers/Dashboard/StudentController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+
+class StudentController extends Controller
+{
+    public function index(): void
+    {
+    }
+
+    public function create(): void
+    {
+    }
+
+    public function store(): void
+    {
+    }
+
+    public function edit(): void
+    {
+    }
+
+    public function update(): void
+    {
+    }
+
+    public function destroy(): void
+    {
+    }
+}
---
+++ app/Http/Requests/StoreStudentRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreStudentRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            "name" => ["required", "max:255"],
+            "surname" => ["required", "max:255"],
+            "index_number" => ["required", "integer", "unique:students,index_number"],
+        ];
+    }
+}
