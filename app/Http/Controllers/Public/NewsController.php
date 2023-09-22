ddb641240eb50066da55a5a42e1da951c4f54287
krzysztofrewak <krzysztof.rewak@blumilk.pl>
2023-09-22 14:49:05 +0200

    #24 - news draft

--- app/Http/Controllers/Public/NewsController.php
+++ app/Http/Controllers/Public/NewsController.php
@@ -5,14 +5,30 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use App\Models\News;
 use Inertia\Response;
 
 class NewsController extends Controller
 {
-    public function __invoke(): Response
+    public function index(): Response
     {
-        return inertia("Public/News", [
-            "title" => "mgr inż.",
+        $news = News::query()
+            ->orderBy("published_at", "desc")
+            ->paginate(10);
+
+        return inertia("Public/News/Index", [
+            "paginator" => $news,
+        ]);
+    }
+
+    public function get(string $slug): Response
+    {
+        $news = News::query()
+            ->where("slug", $slug)
+            ->sole();
+
+        return inertia("Public/News/News", [
+            "news" => $news,
         ]);
     }
 }
---
+++ resources/js/Pages/Public/News/Index.vue
@@ -0,0 +1,48 @@
+<script setup>
+import PublicLayout from '@/Layouts/PublicLayout.vue'
+import BackgroundGrid from '../../../Components/BackgroundGrid.vue'
+import SectionHeader from '../../../Components/SectionHeader.vue'
+
+defineProps({
+  paginator: Object,
+})
+</script>
+
+<template>
+  <PublicLayout>
+    <div class="relative isolate bg-white">
+      <BackgroundGrid />
+      <div class="py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <SectionHeader>
+            <template #header>
+              Aktualności
+            </template>
+            <template #subheader>
+              Ostatnie wiadomości opublikowane w systemie.
+            </template>
+          </SectionHeader>
+
+          <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <article v-for="post in paginator.data" :key="post.id" class="flex max-w-xl flex-col items-start justify-between">
+              <div class="flex items-center gap-x-4 text-xs">
+                <time :datetime="post.published_at" class="text-gray-500">{{ post.published_at }}</time>
+              </div>
+              <div class="group relative">
+                <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a :href="'aktualnosci/' + post.slug">
+                    <span class="absolute inset-0" />
+                    {{ post.title }}
+                  </a>
+                </h3>
+                <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                  {{ post.content }}
+                </p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </PublicLayout>
+</template>
