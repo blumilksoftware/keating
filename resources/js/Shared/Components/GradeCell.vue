2d7177e8fd41078e86b275016c3d80c9dc8e58a2
Kamil <kamil.piech.97@gmail.com>
2023-10-28 09:21:15 +0200

    #12 - cr fix colors

--- resources/js/Shared/Components/GradeCell.vue
+++ resources/js/Shared/Components/GradeCell.vue
@@ -31,14 +31,14 @@ function createGradeOnClick() {
   <TableCell
     v-if="grade"
     :class="[grade?.status
-      ? 'bg-green-300'
-      : grade ? 'bg-red-300' :'bg-white']"
+      ? 'bg-lime-200'
+      : grade ? 'bg-rose-300' :'bg-white']"
     @dblclick="emit('updateGrade',gradeColumn.id, student.id, grade.value, !grade.status)"
   >
     <input
-      :class="[grade.status? 'bg-green-300' : 'bg-red-300']"
+      :class="[grade.status? 'bg-lime-200' : 'bg-rose-300']"
       :value="grade.value"
-      class="h-full w-full cursor-pointer border-0 p-0 text-center font-bold shadow-none ring-0 focus:border-0 focus:ring-0"
+      class="h-full w-full cursor-pointer border-0 p-0 text-center font-bold text-gray-900 shadow-none ring-0 focus:border-0 focus:ring-0"
       @change="emit('updateGrade',gradeColumn.id, student.id, $event.target.value, grade.status)"
     >
   </TableCell>
