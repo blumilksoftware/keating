fa5803a53fc51b08f8250781e504b5b53b3fed6a
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2024-08-20 08:41:36 +0200

    Update resources/js/Pages/Dashboard/Student/Import.vue
    Co-authored-by: Ewelina Skrzypacz <56546832+EwelinaSkrzypacz@users.noreply.github.com>

--- resources/js/Pages/Dashboard/Student/Import.vue
+++ resources/js/Pages/Dashboard/Student/Import.vue
@@ -38,7 +38,7 @@ function importStudents() {
             <FormGroup>
               <FormLabel for="content">
                 Źródło strony z WU
-                <span class="text-gray-500">(uczelna -> protokoły -> edytuj)</span>
+                <span class="text-gray-500">(uczelnia -> protokoły -> edytuj)</span>
               </FormLabel>
               <textarea v-model="form.content" class="block h-[320px] w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6" />
               <FormError :error="form.errors.content" />
