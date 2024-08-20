b3b8e6482123cb00baa8d2fa3807f32ed704cb74
krzysztofrewak <krzysztof.rewak@blumilk.pl>
2024-08-20 10:35:29 +0200

    #78 - GHA debugging

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Models\Setting;
 use Illuminate\Cache\CacheManager;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -20,17 +19,6 @@ class ViewServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $title = app(CacheManager::class)->get("pageTitle");
-
-//        if (!$title) {
-//            /** @var Setting $settings */
-//            $settings = Setting::query()->first();
-//
-//            $title = $settings
-//                ? "{$settings->teacher_titles} {$settings->teacher_name}, {$settings->university_name}"
-//                : config("app.name");
-//
-//            app(CacheManager::class)->put("pageTitle", $title);
-//        }
 
         View::share("pageTitle", $title);
     }
